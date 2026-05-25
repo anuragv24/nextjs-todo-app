3ec085ed8a0f706bde7b573eddb1d77abbfb0e51
Anurag Verma <anuragv984@gmail.com>
2026-05-25 16:07:25 +0530

    feat: Add compiler options to remove console logs in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  }
 };
 
 export default nextConfig;
